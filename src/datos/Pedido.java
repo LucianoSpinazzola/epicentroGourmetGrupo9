2b48bc1272acde31f97749aa8b9d51eb6d92f1e0
LucianoSpinazzola <lucianospin0304@gmail.com>
2026-09-08 20:20:27 -0300

    Corrijo consulta con type() y bug de items null en Pedido

--- src/datos/Pedido.java
+++ src/datos/Pedido.java
@@ -1,15 +1,18 @@
 package datos;
 
 import java.time.LocalDate;
+import java.util.HashSet;
 import java.util.Set;
 
 public class Pedido {
 	private long idPedido;
 	private LocalDate fecha;
 	private UnidadVenta unidadVenta;
-	private Set<Item> items;
+	private Set<Item> items = new HashSet<Item>();
+
 	public Pedido() {
 	}
+
 	public Pedido(long idPedido, LocalDate fecha, UnidadVenta unidadVenta, Set<Item> items) {
 		super();
 		this.idPedido = idPedido;
@@ -17,39 +20,45 @@ public class Pedido {
 		this.unidadVenta = unidadVenta;
 		this.items = items;
 	}
+
 	public long getIdPedido() {
 		return idPedido;
 	}
+
 	protected void setIdPedido(long idPedido) {
 		this.idPedido = idPedido;
 	}
+
 	public LocalDate getFecha() {
 		return fecha;
 	}
+
 	public void setFecha(LocalDate fecha) {
 		this.fecha = fecha;
 	}
+
 	public UnidadVenta getUnidadVenta() {
 		return unidadVenta;
 	}
+
 	public void setUnidadVenta(UnidadVenta unidadVenta) {
 		this.unidadVenta = unidadVenta;
 	}
+
 	public Set<Item> getItems() {
 		return items;
 	}
+
 	public void setItems(Set<Item> items) {
 		this.items.clear();
-		if(items != null) {
+		if (items != null) {
 			this.items.addAll(items);
-	    }
+		}
 	}
+
 	@Override
 	public String toString() {
 		return "Pedido [idPedido=" + idPedido + ", fecha=" + fecha + ", unidadVenta=" + unidadVenta + "]";
 	}
-	
-	
-
 
 }
