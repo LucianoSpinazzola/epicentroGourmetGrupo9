8cf88296c9a1d7346d9cbdf0d5206d2fd10eb7c5
LucianoSpinazzola <lucianospin0304@gmail.com>
2026-09-10 14:43:37 -0300

    Caso de uso Pedido: agregar 4to parametro Festival y fix mapeo Festival

--- src/test/TestPedidosPorFechaYTipo.java
+++ src/test/TestPedidosPorFechaYTipo.java
@@ -3,8 +3,10 @@ package test;
 import java.time.LocalDate;
 import java.util.List;
 
+import datos.Festival;
 import datos.Item;
 import datos.Pedido;
+import negocio.FestivalABM;
 import negocio.PedidoABM;
 
 public class TestPedidosPorFechaYTipo {
@@ -12,14 +14,17 @@ public class TestPedidosPorFechaYTipo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		PedidoABM abm = new PedidoABM();
+		FestivalABM festivalAbm = new FestivalABM();
 
 		LocalDate fechaDesde = LocalDate.of(2026, 1, 1);
 		LocalDate fechaHasta = LocalDate.of(2026, 12, 31);
 		String tipoUnidad = "CamionComida";
+		Festival festival = festivalAbm.traer(1);
 
-		List<Pedido> lista = abm.traerPorFechaYTipoUnidad(fechaDesde, fechaHasta, tipoUnidad);
+		List<Pedido> lista = abm.traerPorFechaYTipoUnidad(fechaDesde, fechaHasta, tipoUnidad, festival);
 
-		System.out.println("Pedidos entre " + fechaDesde + " y " + fechaHasta + " de tipo " + tipoUnidad + ":");
+		System.out.println("Pedidos entre " + fechaDesde + " y " + fechaHasta + " de tipo " + tipoUnidad
+				+ " del festival " + festival.getNombre() + ":");
 		for (Pedido p : lista) {
 			System.out.println("Pedido " + p.getIdPedido() + " - fecha: " + p.getFecha());
 			for (Item i : p.getItems()) {
@@ -28,5 +33,4 @@ public class TestPedidosPorFechaYTipo {
 		}
 
 	}
-
 }
